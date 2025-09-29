7b0bca921fe8e878b7f280b0c9d589615415a679
“Tristian <tristian.davis@atlasschool.com>
2025-09-29 15:56:36 -0500

    vote button incrementing correctly

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,7 @@
 // Define your server actions here
 "use server";
 
+import { incrementVotes } from "./data";
 import { insertQuestion } from "./data";
 import { revalidatePath } from "next/cache";
 import { insertTopic } from "./data";
@@ -32,5 +33,15 @@ export async function addQuestion(question: FormData) {
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to add question.");
+  }
+}
+
+export async function addVote(data: FormData) {
+  try {
+    incrementVotes(data.get("id") as string);
+    revalidatePath("/ui/topics/[id]", "page");
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to add vote.");
   }
 }
